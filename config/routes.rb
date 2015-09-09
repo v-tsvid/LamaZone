808626efaae6038ecfed6446d19887cbab6a7713
v-tsvid <v.tsvid@i.ua>
2015-09-09 15:48:27 +0300

    update some views

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,9 @@ Rails.application.routes.draw do
   resources :ratings
   resources :authors
   resources :categories
-  resources :books
+  resources :books do
+    resources :ratings, shallow: true
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :book do
     title { Faker::Lorem.words(rand(1..7)).join(' ') }
     description { Faker::Lorem.paragraphs(rand(3..5)).join('\n') }
-    price { rand(10.0..100.0) }
+    price { rand(10.0..100.0).round(2) }
     books_in_stock { rand(1..100) }
     author
     category
