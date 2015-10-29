b3d379a8e6610d68f5be3fb0c10ad98099da21af
v-tsvid <v.tsvid@i.ua>
2015-10-29 10:43:44 +0200

    edit local envs for travis ci

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
   
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['S3_KEY'],                        # required
-    aws_secret_access_key: ENV['S3_SECRET'],                        # required
-    region:                ENV['S3_REGION']                  # optional, defaults to 'us-east-1'
+    aws_access_key_id:     ENV['ARTIFACTS_KEY'],                        # required
+    aws_secret_access_key: ENV['ARTIFACTS_SECRET'],                        # required
+    region:                ENV['ARTIFACTS_REGION']                  # optional, defaults to 'us-east-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
