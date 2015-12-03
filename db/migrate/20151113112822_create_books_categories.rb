ddb89d7b61eefd219fc173ed52c0867abe36cab6
v-tsvid <v.tsvid@i.ua>
2015-12-03 16:03:35 +0200

    modify travis file to successfull build

--- db/migrate/20151113112822_create_books_categories.rb
+++ db/migrate/20151113112822_create_books_categories.rb
@@ -1,8 +1,8 @@
 class CreateBooksCategories < ActiveRecord::Migration
   def change
     create_table :books_categories, id: false do |t|
-      t.integer :book_id, index: true, foreign_key: true
-      t.integer :category_id, index: true, foreign_key: true
+      t.references :book, index: true
+      t.references :category, index: true
     end
   end
 end
