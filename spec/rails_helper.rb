6b20bac10de1bd16c20b2e449514f1321130d0b3
v-tsvid <v.tsvid@i.ua>
2016-07-05 11:11:08 +0300

    set up tests for codeclimate

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 require 'simplecov'
 SimpleCov.start 'rails'
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
